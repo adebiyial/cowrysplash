2a72ca90cb1962738636fc93b216a2e92d0480fb
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2019-07-22 23:48:03 +0100

    Add utility functions

--- src/utils/fx.js
+++ src/utils/fx.js
@@ -1,1 +1,5 @@
+import axios from "axios";
+
 export const uuid = (seed = 100) => Math.random().toString(36).substring(2, 2 + seed);
+
+export const BaseUrl = axios.create({ baseURL: `https://api.unsplash.com/photos/search` });
