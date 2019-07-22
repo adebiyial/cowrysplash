c315dd5076d8bd8fa0dd4ae4807881ee22b3db59
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2019-07-22 23:46:25 +0100

    Add fx-nality for image preview modal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app" class="photos-section">
     <Header @search-photos="populatePhotoList" />
-    <Gallery v-bind:photos="photos" />
+    <Gallery
+      v-bind:photos="photos"
+      v-bind:newSearch="newSearch"
+      @preview-image="previewImage"
+    />
+    <Modal
+      v-if="this.showModal"
+      v-bind:photo="photo"
+      @close-modal="closeImagePreviewModal"
+    />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Gallery from "./components/Gallery.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "app",
   components: {
     Header,
-    Gallery
+    Gallery,
+    Modal
   },
   data() {
     return {
-      photos: []
+      photos: [],
+      showModal: false,
+      photo: {},
+      newSearch: true
     };
   },
   methods: {
     populatePhotoList(photoDetails) {
       this.photos = [...photoDetails];
+      this.newSearch = false;
+    },
+    previewImage(id) {
+      this.photo = this.photos.find(photo => photo.id === id);
+      this.showModal = true;
+    },
+    closeImagePreviewModal() {
+      this.showModal = false;
     }
   }
 };
@@ -30,4 +52,5 @@ export default {
 
 <style>
 @import "./styles/global.scss";
+@import "./styles/_normalize.scss";
 </style>
