035398b7d44201f5fd3c2a06d5861a77e178726a
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2019-07-23 00:04:06 +0100

    Add section functionality for Photo

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,21 +1,45 @@
 <template>
-  <a v-bind:href="photo.imgSrc" class="photo-content content">
-    <div class="photo-wrap" data-img="">
-      <img
-        class="photo normal"
-        v-bind:src="photo.imgSrc"
-        v-bind:alt="photo.imgDesc"
-      />
+  <a
+    class="photo-content"
+    v-bind:href="photo.photoSrcLarge"
+    v-if="!loadingImage"
+  >
+    <div class="photo-wrap">
+      <picture>
+        <source
+          v-bind:srcset="photo.photoSrcLarge"
+          media="(min-width: 700px)"
+        />
+        <source
+          v-bind:srcset="photo.photoSrcSmall"
+          media="(max-width: 700px)"
+        />
+        <img
+          class="photo normal"
+          @click="previewImage"
+          v-bind:src="photo.photoSrcSmall"
+          v-bind:alt="photo.photoDesc"
+          v-bind:data-photoId="photo.id"
+        />
+      </picture>
     </div>
     <div class="author-details">
       <h3 class="author-name">{{ photo.authorName }}</h3>
-      <p class="author-location">{{ photo.authorLocation }}</p>
+      <p class="photo-location">{{ photo.photoLocation }}</p>
     </div>
   </a>
 </template>
 
 <script>
 export default {
-  props: ["photo"]
+  props: ["photo", "loadingImage"],
+  methods: {
+    previewImage(e) {
+      // The browser tries to handle the image preview. Stop it!
+      e.preventDefault();
+
+      this.$emit("preview-image", this.photo.id);
+    }
+  }
 };
 </script>
