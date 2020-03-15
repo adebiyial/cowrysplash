4d96aa7dab8e34d1f6745835ecdd98f13c12ba2c
adebiyi adedotun <adebiyi@kobo360.com>
2020-03-15 09:55:08 +0100

    Fix bug in base url

--- src/utils/fx.js
+++ src/utils/fx.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-export const uuid = (seed = 100) => Math.random().toString(36).substring(2, 2 + seed);
+export const uuid = (seed = 100) =>
+  Math.random()
+    .toString(36)
+    .substring(2, 2 + seed);
 
-export const BaseUrl = axios.create({ baseURL: `https://api.unsplash.com/photos/search` });
+export const BaseUrl = axios.create({
+  baseURL: `https://api.unsplash.com/photos/`
+});
